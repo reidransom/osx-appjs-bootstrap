335e18feaa822619c1f0f83f259e36445f779363
Reid Ransom <reid@reidransom.com>
2013-02-12 17:49:24 -0800

    renamed git_it to .get_it and clean it up

--- test.sh
+++ test.sh
@@ -2,8 +2,9 @@
 
 # First, kill the old process by looking for the node from this project and getting its PID.
 get_proc=`ps -e -o pid,command | grep "${DIR}/bin/node"`
-echo $get_proc > get_it
-get_pid=`awk -F" " '{ print $1 }' get_it`
+echo $get_proc > .get_it
+get_pid=`awk -F" " '{ print $1 }' .get_it`
+rm .get_it
 kill -9 $get_pid > /dev/null 2>&1;
 
 
