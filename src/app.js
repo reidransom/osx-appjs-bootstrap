d8c8d69b1a2912afecce88a817578bdd892c76fc
Reid Ransom <reid@reidransom.com>
2013-02-13 19:54:45 -0800

    added hide and hide others keyboard shortcuts, also fixed shortcut keys so Command+Ctl+<some key> isn't the same as Command+<some key>

--- src/app.js
+++ src/app.js
@@ -90,105 +90,271 @@ window.on('create', function(){
   window.frame.setMenuBar(menubar);
 });
 
+
 // add require/process/module to the window global object for debugging from the DevTools
 window.on('ready', function(){
   
+
+  var debuglog = function(msg) {
+    var launched_via_test = false;
+    if (launched_via_test) {
+      console.log(msg);
+    }
+    else {
+      //window.document.getElementsByTagName('body').innerHTML += '<pre>' + msg + '</pre>';
+    }
+  },
+    altdown = false,
+    metadown = false,
+    ctldown = false,
+    shiftdown = false;
+
   window.N = {
     require: require,
     process: process,
     module: module,
     __dirname: __dirname,
     // add node modules here:
-    fs: require("fs"),
-    path: require("path")
+    fs: require('fs'),
+    path: require('path'),
+    child_process: require('child_process')
   };
-  
+
+  var Stream = require('stream');
+
+  function ConsoleStream(con){
+    Stream.call(this);
+    this.readable = false;
+    this.writable = true;
+    this.isRaw = false;
+    this._console = con;
+  }
+
+  ConsoleStream.prototype = {
+    __proto__: Stream.prototype,
+    write: function write(buff, encoding) {
+      if (!this.writable) {
+        this.emit('error', new Error('Tried to write to a closed ConsoleStream'));
+        return false;
+      }
+
+      if (buff instanceof Buffer) {
+        buff = buff.toString(encoding || 'utf8');
+      }
+      this._console.log(buff + '');
+      return true;
+    },
+    end: function end(buff, encoding){
+      if (buff) {
+        this.write(buff, encoding);
+      }
+      this.destroy();
+    },
+    destroy: function destroy(){
+      if (this.writable) {
+        this.writable = false;
+        this._console = null;
+        this.emit('close');
+      }
+    },
+    destroySoon: function destroySoon(){
+      this.destroy();
+    }
+  };
+
+  var stderr = process.stderr;
+  var stdout = process.stdout;
+
+  var consoleStream = new ConsoleStream(window.console);
+  var desc = { configurable: true,
+               enumerable: true,
+               get: function(){ return consoleStream } };
+
+  Object.defineProperties(process, {
+    stderr: desc,
+    stdout: desc
+  });
+
+  var __appname = false;
+  var getAppName = function() {
+    if (__appname) {
+      // Return cached version
+      return __appname;
+    }
+    var info_plist_path = window.N.path.normalize(window.N.path.join(__dirname, "..", "Info"));
+    window.N.child_process.exec('defaults read "' + info_plist_path + '" CFBundleDisplayName',
+      function(error, stdout, stderr) {
+        // Cache the app name
+        if (error) {
+          __appname = "node";
+        }
+        else {
+          __appname = stdout.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+        }
+        return __appname;
+      }
+    );
+  }
+  // Call this just so you have the appname is in the cache
+  getAppName();
+
+  window.addEventListener('keyup', function(e) {
+    if (e.keyIdentifier === 'Meta') {
+      metadown = false;
+    }
+    else if (e.keyIdentifier === 'Alt') {
+      altdown = false;
+    }
+    else if (e.keyIdentifier === 'Control') {
+      ctldown = false;
+    }
+    else if (e.keyIdentifier === 'Shift') {
+      shiftdown = false;
+    }
+  });
+
   window.addEventListener('keydown', function(e){
-    if (e.keyIdentifier === 'F12') {
+    console.log(e.keyIdentifier);
+    if (altdown && ctldown && shiftdown) {
+      // Shift+Control+Alt+Command
+    }
+    else if (shiftdown && altdown) {
+      // Shift+Alt+Command
+    }
+    else if (shiftdown && ctldown) {
+      // Shift+Control+Command
+    }
+    else if (altdown && ctldown) {
+      // Control+Alt+Command
+    }
+    else if (altdown) {
+      // Alt+Command
+      if (e.keyIdentifier === 'U+0048') {
+        // Alt+Command+H
+        
+        window.N.child_process.exec("osascript -e 'tell application \"System Events\" to set visible of every process whose visible is true and name is not \"" + getAppName() + "\" to false'");
+      }
+      else if (e.keyIdentifier === 'U+004A') {
+        // Alt+Command+J
+        window.frame.openDevTools();
+      }
+    }
+    else if (ctldown) {
+      // Control+Command
+    }
+    else if (shiftdown) {
+      // Shift+Command
+    }
+    else if (e.keyIdentifier === 'F12') {
+      // F12
       window.frame.openDevTools();
     }
-    else if (e.keyIdentifier === 'U+0051') {
-      // Command+Q
-      window.close();
-    }
-    else if (e.keyIdentifier === 'U+0057') {
-      // Command+W
-      window.close();
-    }
-    else if (e.keyIdentifier === 'U+0048') {
-      // Command+H
-      console.log('Hide only works when launched from an OSX .app bundle');
-    }
-    else if (e.keyIdentifier === 'U+0058') {
-      // Command+X
-      window.document.execCommand('Cut');
-    }
-    else if (e.keyIdentifier === 'U+0043') {
-      // Command+C
-      window.document.execCommand('Copy');
-    }
-    else if (e.keyIdentifier === 'U+0056') {
-      // Command+V
-      window.document.execCommand('Paste');
-    }
-    else if (e.keyIdentifier === 'U+0041') {
-      // Command+A
-      // (taken from http://stackoverflow.com/questions/6240139/highlight-text-range-using-javascript)
-      var getTextNodesIn = function(node) {
-        var textNodes = [];
-        if (node.nodeType == 3) {
-          textNodes.push(node);
-        } else {
-          var children = node.childNodes;
-          for (var i = 0, len = children.length; i < len; ++i) {
-            textNodes.push.apply(textNodes, getTextNodesIn(children[i]));
-          }
-        }
-        return textNodes;
-      },
-      setSelectionRange = function(el, start, end) {
-        if (window.document.createRange && window.getSelection) {
-          var range = window.document.createRange();
-          range.selectNodeContents(el);
-          var textNodes = getTextNodesIn(el);
-          var foundStart = false;
-          var charCount = 0, endCharCount;
-          for (var i = 0, textNode; textNode = textNodes[i++]; ) {
-            endCharCount = charCount + textNode.length;
-            if (!foundStart && start >= charCount && (start < endCharCount || (start == endCharCount && i < textNodes.length))) {
-              range.setStart(textNode, start - charCount);
-              foundStart = true;
+    else {
+      // Command
+      if (e.keyIdentifier === 'U+0051') {
+        // Command+Q
+        window.close();
+      }
+      else if (e.keyIdentifier === 'U+0057') {
+        // Command+W
+        window.close();
+      }
+      else if (e.keyIdentifier === 'U+0048') {
+        // Command+H
+        var info_plist_path = window.N.path.normalize(window.N.path.join(__dirname, "..", "Info"));
+        window.N.child_process.exec('defaults read "' + info_plist_path + '" CFBundleDisplayName',
+          function(error, stdout, stderr) {
+            var app_name = "node";
+            if (! error) {
+              app_name = stdout.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
             }
-            if (foundStart && end <= endCharCount) {
-              range.setEnd(textNode, end - charCount);
-              break;
-            }
-            charCount = endCharCount;
+            window.N.child_process.exec("osascript -e 'tell application \"System Events\" to set visible of process \"" + getAppName() + "\" to false'");
           }
-          var sel = window.getSelection();
-          sel.removeAllRanges();
-          sel.addRange(range);
-        } else if (window.document.selection && window.document.body.createTextRange) {
-          var textRange = window.document.body.createTextRange();
-          textRange.moveToElementText(el);
-          textRange.collapse(true);
-          textRange.moveEnd("character", end);
-          textRange.moveStart("character", start);
-          textRange.select();
+        );
+      }
+      else if (e.keyIdentifier === 'U+0058') {
+        // Command+X
+        window.document.execCommand('Cut');
+      }
+      else if (e.keyIdentifier === 'U+0043') {
+        // Command+C
+        window.document.execCommand('Copy');
+      }
+      else if (e.keyIdentifier === 'U+0056') {
+        // Command+V
+        window.document.execCommand('Paste');
+      }
+      else if (e.keyIdentifier === 'U+0041') {
+        // Command+A
+        // (taken from http://stackoverflow.com/questions/6240139/highlight-text-range-using-javascript)
+        var getTextNodesIn = function(node) {
+          var textNodes = [];
+          if (node.nodeType == 3) {
+            textNodes.push(node);
+          } else {
+            var children = node.childNodes;
+            for (var i = 0, len = children.length; i < len; ++i) {
+              textNodes.push.apply(textNodes, getTextNodesIn(children[i]));
+            }
+          }
+          return textNodes;
+        },
+        setSelectionRange = function(el, start, end) {
+          if (window.document.createRange && window.getSelection) {
+            var range = window.document.createRange();
+            range.selectNodeContents(el);
+            var textNodes = getTextNodesIn(el);
+            var foundStart = false;
+            var charCount = 0, endCharCount;
+            for (var i = 0, textNode; textNode = textNodes[i++]; ) {
+              endCharCount = charCount + textNode.length;
+              if (!foundStart && start >= charCount && (start < endCharCount || (start == endCharCount && i < textNodes.length))) {
+                range.setStart(textNode, start - charCount);
+                foundStart = true;
+              }
+              if (foundStart && end <= endCharCount) {
+                range.setEnd(textNode, end - charCount);
+                break;
+              }
+              charCount = endCharCount;
+            }
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+          } else if (window.document.selection && window.document.body.createTextRange) {
+            var textRange = window.document.body.createTextRange();
+            textRange.moveToElementText(el);
+            textRange.collapse(true);
+            textRange.moveEnd("character", end);
+            textRange.moveStart("character", start);
+            textRange.select();
+          }
+        },
+        el = window.document.activeElement;
+        if (["textarea", "input"].indexOf(el.tagName.toLowerCase()) != -1) {
+          // todo: check to make sure input element is of type text or password
+          //console.log(el.attributes);
+          el.select();
         }
-      },
-      el = window.document.activeElement;
-      if (["textarea", "input"].indexOf(el.tagName.toLowerCase()) != -1) {
-        // todo: check to make sure input element is of type text or password
-        //console.log(el.attributes);
-        el.select();
+        else {
+          setSelectionRange(window.document.getElementsByTagName("body")[0]);
+        }
+      }
+      else if (e.keyIdentifier === 'Meta') {
+        metadown = true;
+      }
+      else if (e.keyIdentifier === 'Alt') {
+        altdown = true;
+      }
+      else if (e.keyIdentifier === 'Control') {
+        ctldown = true;
+      }
+      else if (e.keyIdentifier === 'Shift') {
+        shiftdown = true;
       }
       else {
-        setSelectionRange(window.document.getElementsByTagName("body")[0]);
+        console.log(e.keyIdentifier);
       }
-    }
-    else {
-      console.log(e.keyIdentifier);
     }
   });
 });
